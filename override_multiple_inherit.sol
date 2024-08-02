36502a1e3e88d3c12055270f4ccc4001a940f584
Unnati gupta <guptaunnati031@gmail.com>
2024-08-02 17:55:37 +0530

    Update override_multiple_inherit.sol

--- override_multiple_inherit.sol
+++ override_multiple_inherit.sol
@@ -1,1 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+//if same name function is present in multiple contract and the child
+//inherits the properties of both the contracts
+//then overriding is done in a bit different way
+
+contract A{
+    uint public a;
+
+    constructor(){
+        a=10;
+    }
+
+    function callA() public{
+        a=100;
+    }
+
+    function cat() pure public virtual returns(string memory){
+        return "I am in A";
+    }
+}
+
+contract B is A{
+    uint public b;
+
+    constructor(){
+        b=20;
+    }
+
+    function callB() public{
+        b=200;
+    }
+
+    function cat() pure public virtual override  returns (string memory){
+        return "I am in B";
+    }
+}
+
+contract C is A,B{
+    
+    function cat() pure public override(B,A) returns (string memory){
+        return "I am in C";
+    }
+}
