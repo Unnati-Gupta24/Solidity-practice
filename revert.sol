ea6b2e174590f2d703c4fe5fd8b98db3c4c4de39
Unnati gupta <guptaunnati031@gmail.com>
2024-08-03 21:51:44 +0530

    Update revert.sol

--- revert.sol
+++ revert.sol
@@ -7,18 +7,25 @@ contract A{
     address public owner = msg.sender;
     uint public age = 25;
 
+    error throwError(string,address);
+
     function check(uint _x) public{
         age = age+5;
         if(_x<2){
-            revert("value is less than 2");
+            revert throwError("value is less than 2",msg.sender);
         }
     }
 
     function ownah() public {
         age = age-2;
         if(owner!=msg.sender){
-            revert("you are not the owner");
+            revert ("you are not the owner");
         }
+    }
+
+    //Assert
+    function checkowner() public view{
+        assert(owner!=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
     }
     
 }
