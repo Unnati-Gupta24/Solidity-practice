59f6a55340969ebb2888c662010864d60ebd5c05
Unnati gupta <guptaunnati031@gmail.com>
2024-08-03 22:51:14 +0530

    Update payable.sol

--- payable.sol
+++ payable.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract A{
+    //while declaring state variables write payable befor public
+    address payable public owner = payable(msg.sender);
+
+    //if a constructor is payable then the deploy button becomes red
+    //and transaction is done at the time of deployment
+    constructor() payable {
+
+    }
+
+    //in case of functions payable can be written
+    //either before or after public
+    function getEth() public payable{
+
+    }
+
+    function checkBalance() public view returns(uint){
+        return address(this).balance;
+    }
+}
